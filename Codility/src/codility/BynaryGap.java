39024950ca77fdf4417908d335c32cb0fbfaa4b8
jesus <jesus@jesus-R430-P430>
2016-01-05 23:08:37 -0500

    Binary gap implementation

---
+++ Codility/src/codility/BynaryGap.java
@@ -0,0 +1,44 @@
+package codility;
+
+
+/** 
+ * @author Jesus Vazquez
+ */
+
+public class BynaryGap {
+    
+    public static void main(String args[]){
+        
+        System.out.println(solution(9));
+        System.out.println(solution(529));
+        System.out.println(solution(15));
+        
+    }
+    
+    public static int solution(int N) {
+        
+        //transform the integer in to a binary representation.
+        String s = Integer.toBinaryString(N);
+        
+        int max = 0;
+        int l = -1;           
+        
+        //compare the last observed position against the current
+        for(int i = 0; i < s.length() ; i++){            
+            if(s.charAt(i) == '1'){
+                if(l == -1)
+                    l = i;
+                
+                else{
+                    if( (i-l-1)>max )
+                        max = i-l-1;
+                    l = i;
+                }                    
+            }            
+        }
+        
+        return max;        
+    }
+    
+    
+}
