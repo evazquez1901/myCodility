a9f35f71c12fcf9b608eda10647dcec6b59c8e11
evazquez1901 <evazquez1901@gmail.com>
2016-02-17 17:20:07 -0500

    Create CyclicRotation.java

---
+++ Arrays/CyclicRotation.java
@@ -0,0 +1,26 @@
+
+/** 
+ * @author Jesus Vazquez
+ *
+ *
+ */
+
+class CyclicRotation {
+  public int[] solution(int[] A, int K) {
+	  if(A.length < 1)
+		  return A;	
+    int n =  A.length;
+    int result[] = new int[n];
+    
+    //Allocate first item.
+    int d = (K>n? K%n :K);        
+    
+    int j = d;                
+    for(int i= 0; i< n ; i++){            
+        if(j>=n)
+            j=0;  
+          result[j++] = A[i];                      
+        }
+      return result;
+    }            
+}
